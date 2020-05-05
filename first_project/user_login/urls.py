1c31e6e099e63704014b76ba88e50a9d02d0f70a
burnerlee <avi.aviral140@gmail.com>
2020-05-06 03:28:57 +0530

    Add login and userDetail features for user

--- first_project/user_login/urls.py
+++ first_project/user_login/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('createUser',views.createUser,name='createUser'),
+    path('signup',views.signup,name='signup'),
+    path('checkUser',views.checkUser,name='checkUser'),
+    path('userDetails/<int:id>',views.userDetails,name='userDetails')
 ]
--- first_project/user_login/views.py
+++ first_project/user_login/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import User
-# Create your views here.
-def index(request):
-    context = {}
-    return render(request,'user_login/signup.html',context)  
 
+def index(request):
+    return render(request,'user_login/login.html',{})  
+
+def signup(request):
+    return render(request,'user_login/signup.html',{})
 
 def createUser(request):
     name = request.POST['name']
@@ -17,3 +18,24 @@ def createUser(request):
         newUser = User(name=name,email=email,password=password)
         newUser.save()
         return HttpResponseRedirect('/user_login') 
+
+def checkUser(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    if len(User.objects.filter(email=email))>0:
+        try:
+            user = User.objects.get(email=email,password=password)
+        except User.DoesNotExist:
+            return HttpResponse("Incorrect password.")
+        return HttpResponseRedirect("userDetails/%s" % user.id)
+    else:
+        return HttpResponse("User with email %s does not exist. Please signup" % email)
+
+def userDetails(request,id):
+    user = User.objects.get(id=id)
+    context = {
+        'name' : user.name,
+        'email' : user.email,
+        'password' : user.password,
+    }
+    return render(request,'user_login/userDetails.html',context)
